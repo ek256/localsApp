f4e3a6089e15da8d235f1d5999837d7b96e84505
Daniel Khan <danielkhan@Daniels-Macbook-Pro.local>
2020-09-03 18:19:35 -0400

    adding css files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "uploads"
+# app.config["UPLOAD_FOLDER"] = "uploads"
 
 @app.route("/")
 def index():
